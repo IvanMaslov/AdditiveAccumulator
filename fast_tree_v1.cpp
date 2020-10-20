b47414ce4af094edb79c6500b9378e3cdf10b74b
Ivan Maslov <maslov.ivan.2000@gmail.com>
2020-10-20 22:22:51 +0300

    Init

---
+++ fast_tree_v1.cpp
@@ -0,0 +1,9 @@
+//
+// Created by maslov on 20.10.20.
+//
+
+#include "fast_tree_v1.h"
+
+int tree::do_some() {
+    return 42;
+}
---
+++ fast_tree_v1.h
@@ -0,0 +1,14 @@
+//
+// Created by maslov on 20.10.20.
+//
+
+#ifndef CRYPTO_FAST_TREE_V1_H
+#define CRYPTO_FAST_TREE_V1_H
+
+struct tree {
+
+    int do_some();
+
+};
+
+#endif //CRYPTO_FAST_TREE_V1_H
